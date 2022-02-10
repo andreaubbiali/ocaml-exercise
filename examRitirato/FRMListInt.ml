8c694288a01f04c00ff39f0246ad9aeb75ac9f2b
aubbiali <aubbiali@sorint.it>
2022-02-10 15:49:10 +0100

    Int list implementation OK

--- examRitirato/FRMListInt.ml
+++ examRitirato/FRMListInt.ml
@@ -1,13 +1,13 @@
 module FRMListInt = struct
 
     type primitive = int;;
-    type t = primitive list;;
+    type t = int list;;
 
     let empty = [];;
 
     let next index lst = List.nth lst index;;
 
-    let has_next index lst = index +1 < (List.length lst);;
+    let has_next index lst = index < (List.length lst);;
 
     let add lst el = el::lst;;
 
--- examRitirato/main.ml
+++ examRitirato/main.ml
@@ -3,14 +3,20 @@ open FRMListInt;;
 
 module Prova = FRM(FRMListInt);;
 
-let aa = Prova.gfilter (fun (y : FRMListInt.primitive) -> y < 5) [5::4];;
+let pp = 1::3::5::4::6::2::[];;
+let aa = Prova.gfilter (fun (y : FRMListInt.primitive) -> y < 5) pp;;
+
+let rec printa_list = function
+    []      -> ()
+|   h::t  -> Printf.printf "%d\n" h; printa_list t;;
 
 let main() = 
-    Printf.printf "ciao\n";;
+    Printf.printf "ciao\n";
+    printa_list aa;;
 
 main();;
 
-(*
+(* PER COMPILARE
 ocamlc FRMInterface.mli FRM.ml FRMListInt.ml
 ocamlc FRM.cmo FRMListInt.cmo main.ml
 *)
